35cbe367bb36d7fb343927db04d17807117cd296
jeremyjv <jeremyjvillanueva@gmail.com>
2024-12-27 13:17:25 -0800

    added redirect to camera from confirmationDialogue

--- sideprojectgg/sideprojectgg/ScanViews/FrontScanView.swift
+++ sideprojectgg/sideprojectgg/ScanViews/FrontScanView.swift
@@ -21,115 +21,114 @@ struct FrontScanView: View {
     @State private var defaultImage: UIImage?
     @State private var photosPickerItem: PhotosPickerItem?
     @State private var showPicker: Bool = false // For PhotosPicker
+    @State private var showCamera: Bool = false // For Selfie
     @State private var analysis: String?
 
     var body: some View {
-        
-        VStack(spacing: 16) {
-            // Top Rectangle
-            RectangleComponent()
-
-                .frame(height: 50) // Adjust height as per your mockup
-            Text("Upload Front Selfie")
+        NavigationStack {
             
-            // Middle Rectangle
-            Image(uiImage: defaultImage ?? UIImage(named: "scanImage")!)
-                .resizable()
-                .scaledToFill()
-                .frame(width: 300, height: 300)
-                .onChange(of: cameraModel.capturedImage) {_, _ in
-                    Task {
-                        defaultImage = cameraModel.capturedImage
-                    }
-                }
-            
-    
-            //Select Photo from camera roll
-            
-            
-            //if default image = UIImage(named: "scanImage") else "retake photo" / "use photo" -> backscanview
-            //have this open as a pmenu instead
-            if defaultImage == nil {
-                Button(action: {
-                    isShowingOptions = true // Show the menu
-                }) {
-                    Text("Upload or take selfie")
-                        .padding()
-                        .frame(maxWidth: .infinity)
-                        .background(Color.blue)
-                        .foregroundColor(.white)
-                        .cornerRadius(10)
-                }
-                .padding()
-                .confirmationDialog("Select an option", isPresented: $isShowingOptions, titleVisibility: .visible) {
+            VStack(spacing: 16) {
+                // Top Rectangle
+                RectangleComponent()
                 
-                        NavigationLink(destination: FrontCameraView(cameraModel: cameraModel)
-                            .ignoresSafeArea()) {
-                                Text("Take a selfie")
-                                    .padding()
-                                    .background(Color.blue)
-                                    .foregroundColor(.white)
-                                    .cornerRadius(10)
-                            }
+                    .frame(height: 50) // Adjust height as per your mockup
+                Text("Upload Front Selfie")
+                
+                // Middle Rectangle
+                Image(uiImage: defaultImage ?? UIImage(named: "scanImage")!)
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: 300, height: 300)
+                    .onChange(of: cameraModel.capturedImage) {_, _ in
+                        Task {
+                            defaultImage = cameraModel.capturedImage
+                        }
+                    }
+                
+                
+                //Select Photo from camera roll
+                //if default image = UIImage(named: "scanImage") else "retake photo" / "use photo" -> backscanview
+                //have this open as a pmenu instead
+                if defaultImage == nil {
+                    Button(action: {
+                        isShowingOptions = true // Show the menu
+                    }) {
+                        Text("Upload or take selfie")
+                            .padding()
+                            .frame(maxWidth: .infinity)
+                            .background(Color.blue)
+                            .foregroundColor(.white)
+                            .cornerRadius(10)
+                    }
+                    .padding()
+                    .confirmationDialog("Select an option", isPresented: $isShowingOptions, titleVisibility: .visible) {
+                        
+                        
+                        Button(action: {
+                            showCamera.toggle()
+                        })
+                        {
+                            Text("Take a selfie")
+                                .padding()
+                                .background(Color.blue)
+                                .foregroundColor(.white)
+                                .cornerRadius(10)
+                        }
+                        
                         Button(action: {
                             showPicker.toggle()
                         }) {
                             Text("Upload Image")
                         }
-                           
-                            
-                }
-                .photosPicker(isPresented: $showPicker, selection: $photosPickerItem)
-                
-                
-                
-                ///
-//                PhotosPicker(selection: $photosPickerItem, matching: .images) {
-//                    Text("-Upload Image-")
-//                }
-//                NavigationLink(destination: FrontCameraView(cameraModel: cameraModel)
-//                    .ignoresSafeArea()) {
-//                    Text("Take Photo")
-//                        .padding()
-//                        .background(Color.blue)
-//                        .foregroundColor(.white)
-//                        .cornerRadius(10)
-//                }
-                
-            } else {
-                Text("-Use Another-") //re
-                NavigationLink(destination: BackScanView()) {
-                    Text("-Continue-")
-                }
-        
-                
-            }
-
-            Spacer()
+                        
+                        
+                    }
+                    .photosPicker(isPresented: $showPicker, selection: $photosPickerItem)
+                    .navigationDestination(isPresented: $showCamera) {
+                            FrontCameraView(cameraModel: cameraModel).navigationBarBackButtonHidden(true)
+                    }
             
-            // Bottom Navigation Bar
-            HStack(spacing: 16) {
-                ForEach(0..<4) { _ in
-                    RectangleComponent()
-                        .frame(width: 50, height: 50) // Adjust size as per your mockup
+                    
+                    
+                    
+                    
+                } else {
+                    Text("-Use Another-") //re
+                    NavigationLink(destination: BackScanView()) {
+                        Text("-Continue-")
+                    }
+                    
+                    
                 }
-            }
-            .padding(.bottom, 16)
-        }
-        .padding()
-        .onChange(of: photosPickerItem) {_, _ in
-            Task {
-                if let photosPickerItem,
-                   let data = try? await photosPickerItem.loadTransferable(type: Data.self) {
-                    if let image = UIImage(data: data) {
-                        defaultImage = image
-                        await viewModel.imageToAnalysis(img: defaultImage!)
+                
+                
+                
+                Spacer()
+                
+                // Bottom Navigation Bar
+                HStack(spacing: 16) {
+                    ForEach(0..<4) { _ in
+                        RectangleComponent()
+                            .frame(width: 50, height: 50) // Adjust size as per your mockup
                     }
                 }
-                photosPickerItem = nil
+                .padding(.bottom, 16)
             }
+            .padding()
+            .onChange(of: photosPickerItem) {_, _ in
+                Task {
+                    if let photosPickerItem,
+                       let data = try? await photosPickerItem.loadTransferable(type: Data.self) {
+                        if let image = UIImage(data: data) {
+                            defaultImage = image
+                            await viewModel.imageToAnalysis(img: defaultImage!)
+                        }
+                    }
+                    photosPickerItem = nil
+                }
+            }
+            
         }
-    
     }
 
 }
