440ad8276f5aa79c1068b99d67c30c8f9c8ba5a8
jeremyjv <jeremyjvillanueva@gmail.com>
2025-01-22 12:43:07 -0800

    got bod arc gold color button correct

--- sideprojectgg/sideprojectgg/ScanViews/ScanEdgeView.swift
+++ sideprojectgg/sideprojectgg/ScanViews/ScanEdgeView.swift
@@ -17,18 +17,30 @@ struct ScanEdgeView: View {
     @State var showPaywall: Bool = false
     @State var showReferral: Bool = false
     var body: some View {
+        
+  
+        
+        
         Button(action: {
+                    // Add your button action here
             showPaywall = true
         }) {
-            
-            Text("🥇Get Body Arc Gold")
+            Text("🥇 Get Body Arc Gold")
+                .font(.system(size: 18, weight: .medium))
+                .foregroundColor(.white)
+                .padding()
+                .frame(maxWidth: .infinity)
+                .background(Color(red: 0.0627, green: 0.4745, blue: 0.6980))
+                .cornerRadius(25)
         }
+        .padding()
         .fullScreenCover(isPresented: $showPaywall) {
             PaywallView(path: $path)
-                .paywallFooter(condensed: true)
+         
             //want to run handleScanUpload and lastGoldScan on a successfull purchase from the paywall footer
         }
-        Text("Invite 3 Friends")
+        
+    
         
         Button(action: {
             showReferral = true
